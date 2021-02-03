b0413d7fb31082984f5f231e77a800283ad66f97
Dimitri <dimitri.despert@gmail.com>
2021-02-03 09:06:49 +0100

    SHOP V1.0.0 - Ajout des front controllers home, product, BDD

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-    echo "hello world";
+    //---FONCTION DEBUG---//
+
+    error_reporting(E_ALL);
+    ini_set('display_errors',true);
+    ini_set('html_errors',false);
+    ini_set('display_startup_errors',true);
+    ini_set('log_errors',false);
+
+    //Variables SANITIZING (URL)
+    $url = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+    $urlIsSet = isset($url);
+
+    //---ROADS---//
+
+    require('routes/web.php');
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,8 @@
 <?php
+
+    //Dictionnaire des routes - ROADS
+    $road = [
+        'home' => 'app/controllers/homeController.php',
+        'product' => 'app/controllers/productController.php',
+        '404' => 'ressources/views/404.tpl',
+    ];
